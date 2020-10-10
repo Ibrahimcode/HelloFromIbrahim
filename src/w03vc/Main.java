486a64c8f8fb33452989f0a90f6857bb241bca35
thecoder <ibrahamacademic@gmail.com>
2020-10-10 19:48:14 +0000

    Just say hello

--- src/w03vc/Main.java
+++ src/w03vc/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("HEllo, my name is Ibrahim");
         System.out.println("Hello everyone, this is Ibrahim");
+        System.out.println("Hello, I am Ibrahim");
     }
 }
