35d22c5fddf241789f46378f00a57b076ae318ca
thecoder <ibrahamacademic@gmail.com>
2020-10-10 19:42:59 +0000

    I just introduce myself with  a greeting

--- src/w03vc/Main.java
+++ src/w03vc/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("HEllo, my name is Ibrahim");
+        System.out.println("Hello everyone, this is Ibrahim");
     }
 }
