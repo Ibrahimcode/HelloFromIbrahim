4c2fb2a5ce96ac275b341d513d7aca4eddd144d5
thecoder <ibrahamacademic@gmail.com>
2020-10-11 20:20:54 +0000

    Respond to Dwain

--- src/w03vc/Main.java
+++ src/w03vc/Main.java
@@ -7,8 +7,9 @@ public class Main {
         System.out.println("HEllo, my name is Ibrahim");
         System.out.println("Hello everyone, this is Ibrahim");
         System.out.println("Hello, I am Ibrahim");
-	System.out.println("Hello Ibrahim, my name is Maksim");
-	System.out.println("Thank you Maksim, I have received it");
-	System.out.println("Hello Ibrahim, my name is Dwain");
+	    System.out.println("Hello Ibrahim, my name is Maksim");
+	    System.out.println("Thank you Maksim, I have received it");
+	    System.out.println("Hello Ibrahim, my name is Dwain");
+	    System.out.println("Thank you Dwain, nice to meet you");
     }
 }
